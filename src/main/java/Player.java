cf0c3b65d91472ca47625f06756ea34458e317cf
mszotek <mszotek@gmail.com>
2019-01-21 14:00:05 +0100

    Player class thumbnail

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,18 @@
-public class Player {
+import java.util.List;
 
+public class Player {
+    private List<Garbage> playerDeck;
+    private int playerScore;
+
+    public Garbage getTopCard(){
+        return null;
+    }
+
+    public void removeTopCard(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("yo");
+    }
 }
