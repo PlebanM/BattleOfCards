e44bb155e11d7fc6d305e2aee08a8dbd177a5ef2
Marcin Pleban <pleban.marcin@gmail.com>
2019-01-22 13:07:21 +0100

    add ValidateInputs class, add methods to View class

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -3,39 +3,75 @@ import java.util.Scanner;
 public class View {
 
     Scanner input;
+    ValidateInputs validate = new ValidateInputs();
 
     public View(){
         this.input = new Scanner(System.in);
     }
 
-    public void showMainMenu(){
-        System.out.println("1. Start game");
-        System.out.println("2. Edit cards");
-        System.out.println("3. End game");
+    public int showMainMenu(){
+        System.out.println("MAIN MENU: \n" +
+                            "1. Start game\n" +
+                            "2. Edit cards\n" +
+                            "3. End game\n");
+        System.out.println("Choose option: ");
 
+        String option = input.next();
+
+        if ((!validate.validationInputInteger(1, 3, option))) {
+            showMainMenu();
+        }
+        return Integer.parseInt(option);
 
     }
+
 
     public void showGameMenu(){
 
     }
 
-    public void showCRUD(){
-        System.out.println("Edit menu. Choose one option: ");
+    public int showCRUD() {
+        System.out.println("Edit menu. Choose one option: \n" +
+                "1. Create new card\n" +
+                "2. Delete card\n" +
+                "3. Edit card\n" +
+                "0. Back\n");
+        System.out.println("Choose option: ");
 
-        System.out.println("1. Create new card");
-        System.out.println("2. Delete card");
-        System.out.println("3. Edit card");
-        System.out.println("4. Back");
 
+        String option = input.next();
+        if ((!validate.validationInputInteger(0, 3, option))) {
+            showCRUD();
+        }
+        return Integer.parseInt(option);
     }
 
-    public void getPositionOnCard(){
+
+
+
+    public int getPositionOnCard(){
         System.out.println("What kind of statistics you want to use? ");
 
+        String option = input.next();
+
+        if ((!validate.validationInputInteger(1, 4, option))) {
+            showMainMenu();
+        }
+        return Integer.parseInt(option);
     }
 
     public void showAllCards(){
 
     }
+
+
+    public void showCard(){
+
+
+
+
+
+    }
+
+
 }
