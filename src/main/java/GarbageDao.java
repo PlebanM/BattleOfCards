642462c4460ac5d2c544696d76e37900c50f077a
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-01-22 02:10:26 +0100

    Added creation of GarbageData table if empty

--- src/main/java/GarbageDao.java
+++ src/main/java/GarbageDao.java
@@ -11,8 +11,10 @@ public class GarbageDao {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:garbageData.db");
         connection.setAutoCommit(false);
+        createTableIfDataFileIsEmpty();
     }
 
+    public void add(Garbage toAdd) {  }
 
 	public Garbage getByID(int index){
 		return new Garbage(1, "test", 0, 0, 0, 0, 0 );
@@ -27,4 +29,25 @@ public class GarbageDao {
 	public boolean updateByID(int id, Garbage item) {
 		return false;
 	}
+
+	private void createTable() {
+        String createTableSqlQuery =
+                "CREATE TABLE GarbageData (\n" +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "NAME TEXT,\n" +
+                "SMELL INT,\n" +
+                "DESIGN INT,\n" +
+                "RECYCLINGTIME INT,\n" +
+                "JUNKVALUE INT,\n" +
+                "WEIGHT INT\n" +
+                ");";
+        statement.executeUpdate(createTableSqlQuery);
+    }
+
+	private void createTableIfDataFileIsEmpty() {
+        resultSet = statement.executeQuery("SELECT * FROM GarbageData");
+        if(resultSet.next() == false) {
+
+        }
+    }
 }
