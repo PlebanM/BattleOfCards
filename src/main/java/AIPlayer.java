4cb8f025b9bfd9442823b7a499bbfd9fd744ef66
mszotek <mszotek@gmail.com>
2019-01-22 10:33:02 +0100

    implemented logic to choose statistic by AI Player

--- src/main/java/AIPlayer.java
+++ src/main/java/AIPlayer.java
@@ -7,6 +7,21 @@ public class AIPlayer extends Player{
 
     @Override
     public int chooseStatisticToCompare() {
-        return 0;
+        Garbage topCard = getPlayerDeck().getTopCard();
+        int statisticNumber = 1;
+        int statisticToCompare = topCard.getSmell();
+        if (topCard.getRecyclingTime() > statisticToCompare){
+            statisticNumber = 2;
+            statisticToCompare = topCard.getRecyclingTime();
+        }
+        if (topCard.getJunkValue() > statisticToCompare){
+            statisticNumber = 3;
+            statisticToCompare = topCard.getJunkValue();
+        }
+        if (topCard.getWeight() > statisticToCompare){
+            statisticNumber = 4;
+            statisticToCompare = topCard.getWeight();
+        }
+        return statisticNumber;
     }
 }
