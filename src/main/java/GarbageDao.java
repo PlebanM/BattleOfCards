49b7e7f69252ae57bc29beff798fa62e85761778
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-01-22 01:16:59 +0100

    Added necesseary fields and constructor to GarbageDao class

--- src/main/java/GarbageDao.java
+++ src/main/java/GarbageDao.java
@@ -1,7 +1,19 @@
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GarbageDao {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public GarbageDao() {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:garbageData.db");
+        connection.setAutoCommit(false);
+    }
+
+
 	public Garbage getByID(int index){
 		return new Garbage(1, "test", 0, 0, 0, 0, 0 );
 	}
