d1ae493105c05473ca72f4cf917aa79fe28d91b3
bq666 <wojciech.gaudnik@gmail.com>
2019-01-23 17:46:50 +0100

    Debuging done

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
 
 
 	public void runGame() {
-		view.showMainMenu();
+//		view.showMainMenu();
 
 		//todo 							"1. Start game\n"
 		boolean isLoadGarbage = serviceGame.loadGarbageFromDB();
@@ -21,20 +21,21 @@ public class Controller {
 		boolean isCardsHandleToPlayers = serviceGame.dealCardsToPlayers(servicePlayer.getPlayers());
 
 		//todo this all part is only for tests
+		testShow();
 		do {
-
-			testShow();
-
 			serviceGame.nextTour(servicePlayer.getPlayers());
-
 			testShow();
-
-
-		} while (!servicePlayer.isWinner());
+			if (servicePlayer.getPlayers().size() == 0) {
+				break;
+			}
+		} while (!servicePlayer.isWinner() || servicePlayer.isAllPlayersEmpty());
 	}
+
+
 
 	private void testShow() {
 		System.out.println("----------------------");
+		System.out.println(servicePlayer.getPlayers().size() + "<--- players size");
 		for (Player player : servicePlayer.getPlayers()) {
 			System.out.println(player.isPlayerTurn() + " : " + player.isActive() + " : " + player.getPlayerName() + " : " + player.getPlayerDeck().getAllCards());
 		}
--- src/main/java/ServiceGame.java
+++ src/main/java/ServiceGame.java
@@ -13,6 +13,73 @@ public class ServiceGame {
 	}
 
 
+	public void nextTour(List<Player> players) {
+		handleLooser(players);
+		Positions choice = this.getChoice(players);
+		for (Player player : players) {
+			if (player.isActive()) {
+				table.addCardToBottom(player.getAndRemoveTopCard());
+			}
+		}
+		List<Integer> winners = this.compareGarbage(choice, table, players);
+		if (winners.size() == 1) {
+			players.get(winners.get(0)).addCardsToBottom(table);
+			players.get(winners.get(0)).setPlayerTurn(true);
+			table.removeAllCards();
+		}
+	}
+
+
+	private Positions getChoice(List<Player> players) {
+		for (Player player : players) {
+			if (player.isActive() && player.isPlayerTurn()) {
+				player.setPlayerTurn(false);
+				return player.chooseStatisticToCompare();
+			}
+		}
+		return Positions.ERROR;
+	}
+
+
+	private List<Integer> compareGarbage(Positions choice, CardsCollection table, List<Player> players) {
+		int max = 0;
+		List<Integer> winNumbers = new ArrayList<>();
+		int numberActivePlayers = 0;
+		for (Player player : players) {
+			numberActivePlayers += (player.isActive() ? 1 : 0);
+		}
+		for (int i = 0; i < numberActivePlayers; i++) {
+			if (table.getCartByID(i).getByChoice(choice) > max) {
+				max = table.getCartByID(i).getByChoice(choice);
+			}
+		}
+		for (int i = 0; i < numberActivePlayers; i++) {
+			if (table.getCartByID(i).getByChoice(choice) == max) {
+				winNumbers.add(i);
+			}
+		}
+		return winNumbers;
+	}
+
+
+	private void handleLooser(List<Player> players) {
+		List<Integer> idToRemove = new ArrayList<>();
+		for (int i = 0; i < players.size(); i++) {
+			if (!players.get(i).isActive()) {
+				players.get(i).showLooseMessage();
+				idToRemove.add(i);
+			}
+		}
+//		System.out.println(idToRemove + " <--- idToRemove");
+//		System.out.println(players + " <--- players");
+		for (int i = idToRemove.size() - 1; i >= 0; i--) {
+			players.remove(i);
+		}
+	}
+
+
+
+
 	public boolean loadGarbageFromDB() {
 		allCards.addCardsFromListToDeck(garbageDao.getAll());
 		return allCards.getSize() != 0;
@@ -33,59 +100,6 @@ public class ServiceGame {
 		}
 		return true;
 	}
-
-
-	public void nextTour(List<Player> players) {
-		Positions choice = this.getChoice(players);
-		for (Player player : players) {
-			if (player.isActive()) {
-				table.addCardToBottom(player.getAndRemoveTopCard());
-			}
-		}
-		List<Integer> winners = this.compareGarbage(choice, table, players);
-		if (winners.size() == 1) {
-			players.get(winners.get(0)).addCardsToBottom(table);
-			players.get(winners.get(0)).setPlayerTurn(true);
-			table.removeAllCards();
-		}
-		for (Player player : players) {
-			if (!player.isActive()) {
-				player.showLooseMessage();
-			}
-		}
-	}
-
-
-	private Positions getChoice(List<Player> players) {
-		for (Player player : players) {
-			if (player.isActive() && player.isPlayerTurn()) {
-				player.setPlayerTurn(false);
-				return player.chooseStatisticToCompare();
-			}
-		}
-		return Positions.ERROR;
-	}
-
-
-	private List<Integer> compareGarbage(Positions choice, CardsCollection table, List<Player> players) {
-		int max = 0;
-		List<Integer> winNumbers = new ArrayList<>();
-		for (int i = 0; i < players.size(); i++) {
-			if (table.getCartByID(i).getByChoice(choice) > max) {
-				max = table.getCartByID(i).getByChoice(choice);
-
-			}
-		}
-		for (int i = 0; i < players.size(); i++) {
-			if (table.getCartByID(i).getByChoice(choice) == max) {
-				winNumbers.add(i);
-			}
-		}
-		return winNumbers;
-	}
-
-
-
 
 
 
