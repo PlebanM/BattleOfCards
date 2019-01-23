f16b36d9fd8cc30437850284731040b669dd8bb7
Marcin Pleban <pleban.marcin@gmail.com>
2019-01-23 11:28:13 +0100

    add new methods MainMenu and CRUD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+        view.addUsersName();
+
+
+    }
 }
--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class View {
@@ -6,14 +9,18 @@ public class View {
     ValidateInputs validate = new ValidateInputs();
 
     public View(){
+
         this.input = new Scanner(System.in);
     }
 
-    public int showMainMenu(){
+    public void showMainMenu() {
         System.out.println("MAIN MENU: \n" +
-                            "1. Start game\n" +
-                            "2. Edit cards\n" +
-                            "3. End game\n");
+                "1. Start game\n" +
+                "2. Edit cards\n" +
+                "3. End game\n");
+    }
+
+    public int chooseMainMenu(){
         System.out.println("Choose option: ");
 
         String option = input.next();
@@ -30,14 +37,16 @@ public class View {
 
     }
 
-    public int showCRUD() {
+    public void showCRUD() {
         System.out.println("Edit menu. Choose one option: \n" +
                 "1. Create new card\n" +
                 "2. Delete card\n" +
                 "3. Edit card\n" +
                 "0. Back\n");
-        System.out.println("Choose option: ");
+    }
 
+    public int chooseCRUDoption(){
+        System.out.println("Choose option: ");
 
         String option = input.next();
         if ((!validate.validationInputInteger(0, 3, option))) {
@@ -55,7 +64,7 @@ public class View {
         String option = input.next();
 
         if ((!validate.validationInputInteger(1, 4, option))) {
-            showMainMenu();
+            getPositionOnCard();
         }
         return Integer.parseInt(option);
     }
@@ -65,13 +74,49 @@ public class View {
     }
 
 
-    public void showCard(){
+    public void showCard(Garbage card){
+
+        StringBuilder strBuild = new StringBuilder();
+        strBuild.append("Card name: " + card.getName())
+                .append(System.getProperty("line.separator"))
+                .append("Smell: " + card.getSmell())
+                .append(System.getProperty("line.separator"))
+                .append("Recykling time: " + card.getRecyclingTime())
+                .append(System.getProperty("line.separator"))
+                .append("Junk value: " + card.getJunkValue())
+                .append(System.getProperty("line.separator"))
+                .append("Weight: " + card.getWeight())
+                .append(System.getProperty("line.separator"));
+    }
+
+    public void addUsersName() {
+        System.out.println("How many users will be playing?");
+        validateAddUsersNames();
+    }
 
 
+     public String[]  validateAddUsersNames(){
+        String usersNumbers = input.next();
+        
 
+        if(!validate.isNumeric(usersNumbers)){
+            addUsersName();
+        }
+
+        int usersNumbersInt = Integer.parseInt(usersNumbers);
+        System.out.println(usersNumbersInt);
+        String[] usersNames = new String[usersNumbersInt];
+
+        for(int i = 0; i < usersNumbersInt; i++ ){
+            System.out.println("Write name of Player number " + (i+1) +": ");
+            usersNames[i]=input.next();
+        }
+         System.out.println(Arrays.toString(usersNames));
+        return  usersNames;
 
 
     }
+    //todo: metoda start game -> lista imion(lista<string>)
 
 
 }
